8fa6ca881819293a064b09997f248dacff1c359b
Al_27 <ahmedlaghfour@gmail.com>
2024-05-15 03:59:04 +0100

    101

---
+++ 0x01-NoSQL/101-students.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+""" Module Documentation """
+
+
+def top_students(mongo_collection):
+    """
+    func
+    """
+    students = [std for students in mongo_collection.find({})]
+    
+    for student in students:
+        name = student.name
+        tot_score = 0
+        for topic in student.topics:
+            tot_score += topic.scor
+        student.update({"averageScore": tot_score/len(student.topics)})
+    
+    return students
+    
